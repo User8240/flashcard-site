843c0964b9912a86929b03b4ee98961c63974dcd
Grace Kostanich <fl5812597@gmail.com>
2022-06-01 20:25:08 -0700

    BUGFIX: Fix click function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $(".definition-showing").toggle();
+    $(".definition-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".definition-showing1").toggle();
+    $(".definition-hidden1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".definition-showing2").toggle();
+    $(".definition-hidden2").toggle();
   });
 });
